31aca84cda5aa4fa9f8d2fefc220545aa1610be2
csalasm1 <36488717+csalasm1@users.noreply.github.com>
2021-12-16 09:16:15 -0500

    Unmodified files

---
+++ signals.py
@@ -0,0 +1,24 @@
+"""Scaffold for signals module. Allows to choose light profile.
+
+TODO:
+
+Check signals module from qiskit's quantum dynamics
+Everything
+Need to connect with model and solver
+"""
+
+import numpy as np
+# import something from scipy?
+
+
+class Signals:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
